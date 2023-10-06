06b86894188cd881d135a7c09097b992b73320c1
naomitesfe <87719049+naomitesfe@users.noreply.github.com>
2023-10-06 07:53:03 +0000

    dynamic pages for each jobs

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import engine, load_jobs_from_db
+from database import engine, load_job_from_db,load_jobs_from_db
 from sqlalchemy import text
 app = Flask(__name__)
 
@@ -14,6 +14,12 @@ def hello_world():
 def jobs_list():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
-             
+
+
+@app.route('/api/jobs/<id>')
+def show_job(id):
+  job = load_job_from_db(id)
+  return render_template('jobpage.html', job=job)
+  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080 , debug=True)
--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, text
 
+
 engine = create_engine("mysql+pymysql://admin:esFQCENu@mysql-148941-0.cloudclusters.net:19360/jjcareers")
 
 def load_jobs_from_db():
@@ -11,4 +12,15 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append(row._asdict())  
     return jobs
-      
+
+    
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+       text(f"SELECT * FROM jobs WHERE id={id}")
+      )
+    jobs = []
+    for row in result.all():
+      jobs.append(row._asdict())
+   
+    return jobs
